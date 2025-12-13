85d52e5a62fc0d368fdc030e550f8ae42f83ab87
bettamulla <raekw1nsandy@gmail.com>
2025-12-13 12:00:56 +0000

    Delete app/components/BrandCard.js

--- app/components/BrandCard.js
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function BrandCard({ brand }) {
-  return (
-    <div className="bg-white p-4 rounded-xl border border-gray-200 shadow-sm">
-      <h3 className="font-medium text-lg">{brand.name}</h3>
-    </div>
-  );
-}
